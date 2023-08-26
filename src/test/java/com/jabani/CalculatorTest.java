cb7cbcc4967f2d8fb33b55a23da7460aa88fdc25
HosseinJavooni <h.javoooni@gmail.com>
2023-08-26 10:21:22 +0330

    1402/06/04_1021am

--- src/test/java/com/jabani/CalculatorTest.java
+++ src/test/java/com/jabani/CalculatorTest.java
@@ -1,14 +1,32 @@
 package com.jabani;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
+
+    private Calculator calculator;
+    @BeforeEach
+    void setUP(){
+        calculator = new Calculator();
+    }
     @Test
     public void canAddZeroPlusZero(){
-        Calculator calculator = new Calculator();
+//        Calculator calculator = new Calculator();
         int sum = calculator.add(0, 0);
         assertEquals(0, sum, "This calculator is OK in Add");
+    }
+    @Test
+    public void onePlusOneCheck(){
+//        Calculator calculator = new Calculator();
+        int sum = calculator.add(1, 1);
+        assertEquals(2, sum, "This calculator is OK in Add");
+    }
+    @Test
+    public void addNegetiveNumbers(){
+        Calculator calculator = new Calculator();
+        assertEquals(-3 , calculator.add(-1, -2));
     }
 }
