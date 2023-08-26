afc1e4b0fea9cd39994c1ab5dc59b6cb723098a6
HosseinJavooni <h.javoooni@gmail.com>
2023-08-26 16:54:54 +0330

    1402/06/04_16:54pm

--- src/test/java/com/jabani/CalculatorTest.java
+++ src/test/java/com/jabani/CalculatorTest.java
@@ -3,7 +3,7 @@ package com.jabani;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
@@ -16,7 +16,7 @@ public class CalculatorTest {
     public void canAddZeroPlusZero(){
 //        Calculator calculator = new Calculator();
         int sum = calculator.add(0, 0);
-        assertEquals(0, sum, "This calculator is OK in Add");
+        assertEquals(0, sum, "When test is rejected this message will be shown");
     }
     @Test
     public void onePlusOneCheck(){
@@ -28,5 +28,17 @@ public class CalculatorTest {
     public void addNegetiveNumbers(){
         Calculator calculator = new Calculator();
         assertEquals(-3 , calculator.add(-1, -2));
+    }
+
+    @Test
+    public void checkSubtractionOfTowNumbers(){
+        int num1 = 2;
+        int num2 = 4;
+        int result = calculator.subtraction(2, 4);
+        //Better performance because string is not prepared for calling the method stack
+        assertEquals(-2, result, () -> num1 + "-" + num2 + " is not equals to " + result);
+        //It's not best practice because string is prepared for calling method even if Test is passd
+        assertEquals(3, result, num1 + "-" + num2 + " is not equals to " + result);
+//        fail("Fail Message");
     }
 }
